180d15b7208691a52693c23f805eb26139437acb
dan933 <69749413+dan933@users.noreply.github.com>
2022-06-12 14:32:43 +1000

    Working DB based on ERD

--- DatabaseDeploymentScript/deploymentScript.sql
+++ DatabaseDeploymentScript/deploymentScript.sql
@@ -9,114 +9,78 @@ DROP TABLE IF EXISTS [Owner];
 
 GO
 
-CREATE TABLE [Owner]
+CREATE TABLE [OWNER]
 (
-    OwnerId INT IDENTITY(1,1) PRIMARY KEY,
-    Surname NVARCHAR(MAX) NOT NULL,
-    Firstname NVARCHAR(MAX) NOT NULL,
-    Phone NVARCHAR(MAX) NOT NULL
+    OwnerId INT PRIMARY KEY,
+    Surname NVARCHAR(300) NOT NULL,
+    Firstname NVARCHAR(300) NOT NULL,
+    Phone NVARCHAR(300) NOT NULL
 );
 
 GO
 
 CREATE TABLE PET
 (
-    PetId INT IDENTITY(1,1) PRIMARY KEY,
-    PetName NVARCHAR(MAX),
-    [Type] NVARCHAR(MAX)
-);
-
-GO 
-
-CREATE TABLE PETSelection
-(
-    PetSelectionId INT IDENTITY(1,1) PRIMARY KEY,
-    OwnerId INT FOREIGN KEY REFERENCES [OWNER],
-    PetId INT FOREIGN KEY REFERENCES PET,
-    CONSTRAINT AK_PETSelection UNIQUE(PetId)
-    
+    OwnerId INT FOREIGN KEY REFERENCES [OWNER],    
+    PetName NVARCHAR(300),
+    [Type] NVARCHAR(300)
+    Constraint PK_PET Primary Key (OwnerId, PetName)
 );
 
 GO
 
 CREATE TABLE [PROCEDURE]
 (
-    ProcedureID INT IDENTITY(1,1) PRIMARY KEY,
-    [DESCRIPTION] NVARCHAR(MAX),
-    Price FLOAT    
+    ProcedureID INT PRIMARY KEY,
+    [Description] NVARCHAR(300),
+    Price MONEY
 );
 
 GO
 
 CREATE TABLE TREATMENT
 (
-    TreatmentID INT IDENTITY(1,1) PRIMARY KEY,    
-    PetId INT FOREIGN KEY REFERENCES PETSelection,
-    ProcedureID INT FOREIGN KEY REFERENCES [PROCEDURE],
+    OwnerID INT,
+    PetName NVARCHAR(300),
+    ProcedureID INT FOREIGN key REFERENCES [PROCEDURE],
     [Date] DATE,
-    Notes NVARCHAR(MAX),    
-    Payment FLOAT    
+    Notes NVARCHAR(300),
+    Payment MONEY
+    Constraint FK_PET FOREIGN Key (OwnerID, PetName) REFERENCES PET
 );
 
 GO
 
-INSERT INTO [dbo].[Owner]
-           ([Surname]
-           ,[Firstname]
-           ,[Phone])
-     VALUES
-           ('Sinatra', 'Frank', '400111222'),
-           ('Ellington', 'Duke', '400222333'),
-           ('Fitzgerald', 'Ella', '400333444')
+INSERT INTO [OWNER] (OwnerID,Surname,Firstname,Phone )
+VALUES
+    ('1', 'Sinatra', 'Frank', '400111222 '),
+    ('2', 'Ellington', 'Duke', '400222333 '),
+    ('3', 'Fitzgerald', 'Ella', '400333444 ');
+
 GO
-
-INSERT INTO [dbo].[PET]
-           ([PetName]
-           ,[Type])
-     VALUES
-           ('Buster', 'Dog'),
-           ('Fluffy', 'Cat'),
-           ('Stew', 'Rabbit'),
-           ('Buster', 'Echidna')
-
 INSERT INTO [dbo].[PROCEDURE]
-           ([DESCRIPTION]
-           ,[Price])
-     VALUES
-           ('Rabies Vaccination',24.00),
-           ('Examine and Treat Wound', 30),
-           ('Heart Worm Test', 25),
-           ('Tetnus Vaccination', 17)
+			([ProcedureID], [Description],[Price])
+VALUES
+	(1, 'Rabies Vaccination', 24.00 ),
+	(10, 'Examine and Treat Wound', 30.00),
+	(5, 'Heart Worm Test', 25.00 ),
+    (8, 'Heart Worm Test', 25.00 );
+           
 GO
 
-INSERT INTO [dbo].[PETSelection]
-           ([OwnerId]
-           ,[PetId])
-     VALUES
-           (1, 1),
-           (1, 2),
-           (2, 3),
-           (3, 4)
+INSERT INTO PET (OwnerID,PetName,Type )
+VALUES
+    (1, 'Buster', 'Dog '),
+    (1, 'Fluffy', 'Cat '),
+    (2, 'Stew', 'Rabbit '),
+    (3, 'Buster', 'Echidna ');
+
 GO
 
-INSERT INTO [dbo].[TREATMENT]
-           (         
-            [PetId]
-            ,[ProcedureID]
-            ,[Date]
-            ,[Notes]
-            ,[Payment]
-           )
-     VALUES
-        (1, 1, '20-Jun-17', 'Routine Vaccination', ' 22.00  '),
-        (1, 2, '15-May-18', 'Booster Shot', ' 24.00  '),
-        (2, 2, '10-May-18', 'Wounds sustained in apparent cat fight.', ' 30.00  '),
-        (3, 2, '10-May-18', 'Wounds sustained during attempt to cook the stew.', ' 30.00  '),
-        (4, 4, '20-Jun-17', 'Stepped on a Rusty Nail', ' 17.00  '),
-        (4, 1, '20-Jun-17', 'Routine Vaccination', ' 22.00  ');
-
-
---SELECT o.*, p.*
---FROM [Owner] AS O
---INNER JOIN PETSelection as ps ON ps.OwnerId = o.OwnerId
---INNER JOIN PET as p ON p.PetId = ps.PetId;
+INSERT INTO TREATMENT ([OwnerID],[PetName],[ProcedureID],[Date],[Notes],[Payment] )
+VALUES
+    (1, 'Buster', 1, '20-Jun-17', 'Routine Vaccination', ' 22.00  '),
+    (1, 'Buster', 1, '15-May-18', 'Booster Shot', ' 24.00  '),
+    (1, 'Fluffy', 10, '10-May-18', 'Wounds sustained in apparent cat fight.', ' 30.00  '),
+    (2, 'Stew', 10, '10-May-18', 'Wounds sustained during attempt to cook the stew.', ' 30.00  '),
+    (3, 'Buster', 8, '20-Jun-17', 'Stepped on a Rusty Nail', ' 17.00  ');
