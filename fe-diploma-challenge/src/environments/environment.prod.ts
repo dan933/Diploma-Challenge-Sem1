eaa3b94e08f9d78bc30f2502af7dc577f426cf6b
dan933 <69749413+dan933@users.noreply.github.com>
2022-06-18 09:00:48 +1000

    working db ARM tamplate

--- fe-diploma-challenge/src/environments/environment.prod.ts
+++ fe-diploma-challenge/src/environments/environment.prod.ts
@@ -2,6 +2,10 @@ export const environment = {
   production: true,
   AUTH0:{
     domain: 'dev-tt6-hw09.us.auth0.com',
-    clientId: 'WvEpucsSOwsMHpHAAS05qoUXi1JAUHS2'
-  }
+    clientId: 'WvEpucsSOwsMHpHAAS05qoUXi1JAUHS2',
+    redirectUri: 'https://localhost:4200/pets',
+    logoutURL: 'https://localhost:4200/login',
+    audience: 'https://diploma-challenge-sem-1.com.au'
+  },
+  apiURL:'https://localhost:7235'
 };
