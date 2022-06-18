c471f6e10a4d4ab76c578cc5c83898c8940cde52
dan933 <69749413+dan933@users.noreply.github.com>
2022-06-18 15:14:09 +1000

    added secrets to appsettings.json

--- API/Helpers/ManagementAPIHelper.cs
+++ API/Helpers/ManagementAPIHelper.cs
@@ -19,9 +19,9 @@ namespace API.Helpers
             request.AddHeader("content-type", "application/json");
             request.AddParameter("application/json", "{\"client_id\":\"kUAAhoahZIBdb6SMoQZbryn9fZ6WIbsy\",\"client_secret\":\"zTHQ3l3jxD_coV1WO5xJGe1GyXOdZfwapI54k-EPLc3l4NuuyHAvm9c1UpwlObhN\",\"audience\":\"https://dev-tt6-hw09.us.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
             request.AddBody(new{
-                client_id ="kUAAhoahZIBdb6SMoQZbryn9fZ6WIbsy",
-                client_secret="zTHQ3l3jxD_coV1WO5xJGe1GyXOdZfwapI54k-EPLc3l4NuuyHAvm9c1UpwlObhN",
-                audience="https://dev-tt6-hw09.us.auth0.com/api/v2/",
+                client_id = configuration["Auth0:ManagementClientID"],
+                client_secret= configuration["Auth0:ManagementClientSecret"],
+                audience= configuration["Auth0:ManagementAudience"],
                 grant_type="client_credentials"
             });
             RestResponse tokenResponse = client.Execute(request);
